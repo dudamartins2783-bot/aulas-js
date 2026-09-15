1faec9f5d3169e81c0b21c8b35cdbd845132019a
maria <leonardomachadopereiramoro@gmail.com>
2026-09-15 11:47:13 -0300

    adicionada pasta 15.09 - variaveis

---
+++ 1509/script.js
@@ -0,0 +1,41 @@
+// let miranha="peter parker";
+// let numero=999999;
+// let munero2= "99999";
+// let idade =17; 
+// var nome  = "odirlei";
+// nome ="pedro"
+// var nome ="odirlei";
+// nome ="pedro";
+// idade=20;
+// idade=21;
+// nome = "pedro";
+
+
+// console.log( "o miranha é o " + miranha "e ele tem" + idade + " anos") ;
+//crie 4 variaveis contendo : nome , sobrenome , idade e curso
+// exiba no console  em umas  unica frase
+let nome = " maria eduarda "
+let sobrenome = " martins "
+let idade = 16
+let curso = " script.js "
+
+// console.log("ola meu nome e " + nome + sobrenome + " e tenho " + idade +  " anos " + " to fazendo " + curso) ;
+
+
+// let nome ="maria eduarda"; //string
+// let peso = 60;//int;
+// let altura = 1.69;
+// let funcionario = false;//bool - true = verdadeiro ou false ==falso
+
+// let cidade = null;
+// let estado;
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
+console.log(" a variavel nome tem " + nome +" e é  do tipo " + typeof nome);
+console.log(" a variavel peso tem " + sobrenome + " e é do tipo " + typeof peso);
+console.log(" a variavel altura tem " + idade +"  e  é do tipo " + typeof altura);
+console.log(" a variavel funcionario tem " + curso + " e é  do tipo " + typeof funcionario);
+

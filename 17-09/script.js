d3bba7fe3baf7bb73fddb869ee02b9a58a386863
maria <leonardomachadopereiramoro@gmail.com>
2026-09-17 11:59:02 -0300

    exercicios aula 17 finalizado

---
+++ 17-09/script.js
@@ -0,0 +1,96 @@
+//let nota = 6;
+//if (nota < 5) {
+    //console.log ("henique reprovado");
+
+//}// else if (nota < 7) {
+   //console.log("henique aprovado");
+
+//} //else {
+   // console.log("henique aprovado");
+//}
+
+let temperatura = 26;
+
+if (temperatura > 30) {
+    console.log("esta quente");
+
+} else if (temperatura > 20 && temperatura <=30) {
+    console.log("temperatura agredavel");
+} else {
+    console.log("esta frio");
+}
+
+let velocidade = 71;
+
+if ("velocidade > 60") {
+    console.log("velocidade permitita");
+
+} else if (velocidade > 60 && quantidadesEstoque< 10 ) {
+   console.log ("ateção:velocidade acima do permitita");
+    console.log("velocidade muito alta");
+} 
+let quantidadesEstoque = 10
+if (quantidadesEstoque == 0) {
+    console.log("produto esgosta");
+
+} if (quantidadesEstoque >1 && quantidadesEstoque >10) {
+     console.log(" estoque disponivel");
+} else  {
+    console.log(" estoque abaixo ");
+}
+ 
+
+let user= "admin"
+let senha = "1234"
+
+if (user == "admin" && senha == "1234" ) {
+    console.log("login relizar com sucesso");
+
+} else {
+   console.log("usuario e senha incorretos");
+}
+
+let hora = 15;
+if (hora <= 0 && hora <= 11) {
+    console.log("bom dia")
+} else if (hora<= 12 && hora <=17) {
+    console.log("boa tarde")
+} else if(hora <=18 && hora <=23) {
+console.log ("boa noite")
+
+} 
+
+
+let nota= 3;
+if (nota == 5) {
+
+    console.log("excelente");
+
+} else if (nota == 4) {
+    console.log("muito bom");
+
+} else if (nota == 3) {
+    console.log("bom");
+
+} else if (nota == 2) {
+    console.log|("regular");
+
+
+}  if (nota==1) {
+    console.log("ruim");
+
+} else {
+   console.log("nota invalida"); 
+} 
+    
+let tipoIngresso = "inteira"
+if (tipoIngresso == "meia") {
+    console.log("R$20");
+} else if (tipoIngresso == "inteira") {
+    console.log("R$40");
+} else if (tipoIngresso == "inteira") {
+    console.log("R$80");
+} else {
+    console.log("Tipo de ingresso invalido");
+} 
+
